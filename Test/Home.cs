75b25b84e2210053a550bc0ffb51e57b19ea3640
Grand Julivan <grandjulivan@gmail.com>
2018-07-23 11:59:20 +0800

    Add files via upload

---
+++ Test/Home.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Web;
+using Xunit;
+
+namespace Test
+{
+    public class Home
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        public Home()
+        {
+
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Index()
+        {
+
+        }
+    }
+}
---
+++ Test/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+    public class Users
+    {
+        [Fact]
+        public void Register()
+        {
+
+        }
+
+        [Fact]
+        public void Login()
+        {
+
+        }
+    }
+}
